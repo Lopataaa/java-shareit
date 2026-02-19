7ddbee6339d5726d598ee2709986b2013d5bf424
User Oksana <oks321mih@yandex.ru>
2026-02-19 21:25:39 +0300

    Переименовала папки в shareit-gateway и shareit-server

--- shareIt-server/src/main/java/ru/practicum/shareit/item/CommentService.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.item;
-
-import ru.practicum.shareit.item.dto.CommentDto;
-
-public interface CommentService {
-    CommentDto createComment(Long itemId, Long userId, CommentDto commentDto);
-}
